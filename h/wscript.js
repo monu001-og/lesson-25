0b00257e188831928706d2d75ba0c5b0ffa2d0cd
monu001-og <olatudeogbe@gmail.com>
2024-10-16 18:15:23 +0100

    index.html

---
+++ h/wscript.js
@@ -0,0 +1,7 @@
+function myfunction() {
+    document.getElementById("result").innerHTML =
+    document.getElementById("demo").firstChild.nodValue;
+
+    document.getElementById("result1").innnerHTML=
+    document.getElementById("demo").childNodes[0].NodeValue;
+}
